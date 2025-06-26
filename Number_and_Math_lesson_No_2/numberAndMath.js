dab37e337ad8f0bea05fe62ff4f37d7445a6828a
Afaq-Ahmad-eng <afaqahmad2593@gmail.com>
2025-06-26 20:14:05 +0500

    Complete Number and Math Exercises (Lesson No 2)

---
+++ Number_and_Math_lesson_No_2/numberAndMath.js
@@ -0,0 +1,37 @@
+// Task No 1
+// console.log(`Your Total of 1 Soup, 3 Burgers and one Ice-cream is $${10 + 8 + 5}`);
+
+// Task No 2
+// console.log(`Your Total of 3 Soup, 9 Burgers and 3 Ice-cream for 3 persons is $${(10 + 8 + 5)*3}`);
+
+// Task No 3
+// console.log(`Your Total of a Toaster = $${18.50} and 2 T-shirt = $${7.50 * 2} is $${18.50 + (7.50 * 2)}`);
+
+// Task No 4
+// console.log(`Your Total of a Toaster = $${18.50} and 2 T-shirt = $${7.50 * 2} and the 10% tax = $${(18.50 + (7.50 * 2))  * (10/100)}  is $${18.50 + (7.50 * 2) + (18.50 + (7.50 * 2))  * (10/100)}`);
+
+// Task No 5
+// console.log(`Your Total of a Toaster = $${18.50} and 2 T-shirt = $${7.50 * 2} and the 20% tax = $${(18.50 + (7.50 * 2))  * (20/100)}  is $${18.50 + (7.50 * 2) + (18.50 + (7.50 * 2))  * (20/100)}`);
+
+// Task No 6 
+// console.log(`A Toaster = $${18.99} and 1 T-shirt = $${7.99} and one Basketball = $${20.95} and shipping for the toaster = $${4.99} Total Before Tax = $${(Math.round((1899 + 799 + 2095)) / 100) + 4.99}`);
+
+// Task No 7 and 8
+// console.log(`A Toaster = $${18.99} and 1 T-shirt = $${7.99} and one Basketball = $${20.95} and shipping for the toaster = $${4.99}  and the 10% Tax = $${Math.round((1899 + 799 + 2095 + 499) * 10/100)/ 100} Order Total = $${(Math.round((1899 + 799 + 2095)) / 100) + 4.99 + Math.round((1899 + 799 + 2095 + 499) * 10/100)/ 100}`);
+
+// Task No 9 remove toaster
+// console.log(`1 T-shirt = $${7.99} and one Basketball = $${20.95}  and the 10% Tax = $${Math.round((799 + 2095) * 10/100)/ 100} Order Total = $${(Math.round(799 + 2095) + Math.round((799 + 2095) * 10/100))/100}`);
+
+// Task No 10 round a number Down
+
+// Task No 11 round a number Up
+// console.log(`1 T-shirt = $${Math.ceil(7.99)} and one Basketball = $${Math.ceil(20.95)}  and the 10% Tax = $${Math.ceil((799 + 2095) * 10/100)/ 100} Order Total = $${Math.ceil((Math.round(799 + 2095) + Math.round((799 + 2095) * 10/100))/100)}`);
+
+// Task No 12 
+// console.log(`Temperature is given in Cesius 25c Convert this into Fahrenheit = ${(25 * 9/5)+32}F`);
+
+// Task No 13
+// console.log(`Temperature is given in Fahrenheit 86F Convert this into Cesius = ${(86 - 32) * 5/9}C`);
+
+// Task No 14
+console.log(`Temperature is given in Cesius -5C Convert this into Fahrenheit = ${(-5 * 9/5) + 32}F`);
