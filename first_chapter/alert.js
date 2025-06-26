9304a2ede27f842bc411f72e102fca7f036b777c
Afaq-Ahmad-eng <afaqahmad2593@gmail.com>
2025-06-26 12:34:19 +0500

    We complete our second task showing name using ALert/Popup

--- first_chapter/alert.js
+++ first_chapter/alert.js
@@ -1,2 +1,5 @@
-// Task No Show Good morning message using alert
-alert("Good Morning!")
+// Task No 1 Show Good morning message using alert
+// alert("Good Morning!")
+
+// Task No 2 show name in popup means using Alert
+alert("My name is Afaq Ahmad Ali Khan Kundi!")
